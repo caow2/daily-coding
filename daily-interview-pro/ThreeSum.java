44332a3e203e1dbdc6285fba3300436a38531ad5
caow2 <caow2@students.rowan.edu>
2019-09-04 16:06:45 -0400

    Comments

--- daily-interview-pro/ThreeSum.java
+++ daily-interview-pro/ThreeSum.java
@@ -20,7 +20,17 @@ public class ThreeSum {
   }
 
   /**
-   * O(n^2) time
+   * To solve 3 sum, we can use the solution to a simpler version of the problem: 2 sum.
+   * For 2 sum, we want to find 2 numbers such that a + b = 0.
+   * We can slightly tweak the solution for 3 sum since we want a + b + c = 0.
+   * i.e. For each number a from arr[0], ..., arr[length - 2], solve 2 sum for b + c = -a.
+   *
+   * Since we want to avoid duplicates, we can either use a HashSet of Lists to maintain
+   * the unique solutions.
+   * Alternatively, we can sort the input and take care of duplicates manually, since
+   * we don't increase the runtime complexity by sorting.
+   *
+   * O(n^2) time.
    */
   public static List<List<Integer>> threeSum(int[] arr) {
     Arrays.sort(arr);
